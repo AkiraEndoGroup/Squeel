dd78fee776812687e01310e6c08e638d5518125d
Gustavo Fulton <ferna194@msu.edu>
2017-11-16 14:50:25 -0500

    Fixed user auth error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,31 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from '../pages/welcome/welcome';
 
+import { Angular2Apollo } from 'angular2-apollo';
+import gql from 'graphql-tag';
+import 'rxjs/add/operator/toPromise';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = WelcomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    if (window.localStorage.getItem('graphcoolToken')) {
-      this.rootPage = TabsPage;
-    }
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public apollo: Angular2Apollo) {
+    //Making sure the user is logged in.
+    this.apollo.query({
+      query: gql`
+        query {
+          user {
+            id
+          }
+        }
+      `
+    }).toPromise().then(({data}) => {
+      if (data && window.localStorage.getItem('graphcoolToken')) {
+        this.rootPage = TabsPage;
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
